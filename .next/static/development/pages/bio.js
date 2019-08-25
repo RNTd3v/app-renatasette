120ead3539a0e6d5cb7ffc328525fbb4a5542cd5
Renato Andrade <renato@rntdesign.com.br>
2019-08-25 19:21:23 -0300

    init project

---
+++ .next/static/development/pages/bio.js
@@ -0,0 +1,94 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/bio.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fbio&absolutePagePath=%2FUsers%2Frntdesign%2FSites%2FRNTDesign%2FRenata%20Sette%2Fapp-renata-sette%2Fpages%2Fbio.js!./":
+/*!*************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fbio&absolutePagePath=%2FUsers%2Frntdesign%2FSites%2FRNTDesign%2FRenata%20Sette%2Fapp-renata-sette%2Fpages%2Fbio.js ***!
+  \*************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/bio", function() {
+      var page = __webpack_require__(/*! ./pages/bio.js */ "./pages/bio.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/bio.js */ "./pages/bio.js", function() {
+          if(!next.router.components["/bio"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/bio.js */ "./pages/bio.js")
+          next.router.update("/bio", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_01f9a3fa864a7b7414d8 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_01f9a3fa864a7b7414d8 */ "dll-reference dll_01f9a3fa864a7b7414d8"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/bio.js":
+/*!**********************!*\
+  !*** ./pages/bio.js ***!
+  \**********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return About; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/rntdesign/Sites/RNTDesign/Renata Sette/app-renata-sette/pages/bio.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+function About() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "This is the about page"));
+}
+
+/***/ }),
+
+/***/ 2:
+/*!*****************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fbio&absolutePagePath=%2FUsers%2Frntdesign%2FSites%2FRNTDesign%2FRenata%20Sette%2Fapp-renata-sette%2Fpages%2Fbio.js ***!
+  \*****************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fbio&absolutePagePath=%2FUsers%2Frntdesign%2FSites%2FRNTDesign%2FRenata%20Sette%2Fapp-renata-sette%2Fpages%2Fbio.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fbio&absolutePagePath=%2FUsers%2Frntdesign%2FSites%2FRNTDesign%2FRenata%20Sette%2Fapp-renata-sette%2Fpages%2Fbio.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_01f9a3fa864a7b7414d8":
+/*!*******************************************!*\
+  !*** external "dll_01f9a3fa864a7b7414d8" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_01f9a3fa864a7b7414d8;
+
+/***/ })
+
+},[[2,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=bio.js.map
