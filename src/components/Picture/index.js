81eaa0163e5c4a736d53903e7068ea8cac06f1bf
Renato Andrade <renato@rntdesign.com.br>
2019-09-10 19:29:48 -0300

    Ajustes hover imagens

--- src/components/Picture/index.js
+++ src/components/Picture/index.js
@@ -5,18 +5,17 @@ class Picture extends Component {
   render() {
     const { src, link, text, title } = this.props;
     return (
-      <figure className="picture">
-        <img src={src} alt={title} className="image" />
-        <figcaption>
-          <div className="textWrapper">
-            <h2 className="title">{title}</h2>
-            <p className="text">{text}</p>
-          </div>
-          <Link href={link}>
-            <a className="link">ver mais</a>
-          </Link>
-        </figcaption>
-      </figure>
+      <Link href={link}>
+        <figure className="picture">
+          <img src={src} alt={title} className="image" />
+          <figcaption>
+            <div className="textWrapper">
+              <h2 className="title">{title}</h2>
+              <p className="text">{text}</p>
+            </div>
+          </figcaption>
+        </figure>
+      </Link>
     );
   }
 }
