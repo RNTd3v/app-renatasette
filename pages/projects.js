bd9888272b35f3ceae5ff415dcf2c2ac06775405
Renato Andrade <renato@rntdesign.com.br>
2019-09-16 07:06:17 -0300

    Criada a pagina de projetos

---
+++ pages/projects.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Link from "next/link";
+import "../src/styles/main.scss";
+
+import Head from "next/head";
+
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
+
+import ProjectMock from "../src/mocks/projects";
+
+const Projects = () => {
+  const projects = ProjectMock;
+  return (
+    <div>
+      <Head>
+        <title>Renata Sette - Projects</title>
+      </Head>
+      <section>
+        <Header />
+        <main className="content project-list">
+          <h1 className="title">Projects</h1>
+          <div className="list">
+            {projects.map((p, i) => {
+              <div className="item" key={i}>
+                <div className="picture">
+                  <img src={p.picture} alt={p.nameEN} />
+                </div>
+                <div className="text">
+                  <h2 className="title">{p.nameEN}</h2>
+                  <p className="description">{p.descriptionEN}</p>
+                  <Link
+                    href="/project/[projectCode]"
+                    as={`/project/${p.codeEN}`}
+                  >
+                    ver mais
+                  </Link>
+                </div>
+              </div>;
+            })}
+          </div>
+        </main>
+        <Footer />
+      </section>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ src/mocks/projects.js
@@ -0,0 +1,44 @@
+const ProjectMock = [
+  {
+    id: "1",
+    codePT: "projeto-um",
+    codeEN: "project-one",
+    namePT: "Projeto 1",
+    nameEN: "Project 1",
+    descriptionPT: "Descrição 1",
+    descriptionEN: "Description 1",
+    picture: "/static/img/photo1.jpg"
+  },
+  {
+    id: "2",
+    codePT: "projeto-dois",
+    codeEN: "project-two",
+    namePT: "Projeto 2",
+    nameEN: "Project 2",
+    descriptionPT: "Descrição 2",
+    descriptionEN: "Description 2",
+    picture: "/static/img/photo2.jpg"
+  },
+  {
+    id: "3",
+    codePT: "projeto-tres",
+    codeEN: "project-three",
+    namePT: "Projeto 3",
+    nameEN: "Project 3",
+    descriptionPT: "Descrição 3",
+    descriptionEN: "Description 3",
+    picture: "/static/img/photo3.jpg"
+  },
+  {
+    id: "4",
+    codePT: "projeto-quatro",
+    codeEN: "project-four",
+    namePT: "Projeto 4",
+    nameEN: "Project 4",
+    descriptionPT: "Descrição 4",
+    descriptionEN: "Description 4",
+    picture: "/static/img/photo4.jpg"
+  }
+];
+
+export default ProjectMock;
