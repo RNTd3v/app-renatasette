26852b03b903bb765cabdeb7f826deed7013552d
Renato Andrade <renato@rntdesign.com.br>
2019-11-06 08:27:13 -0300

    tt

--- env-config.js
+++ env-config.js
@@ -3,7 +3,6 @@ module.exports = {
   "process.env.API_URL": prod
     ? "https://api.renatasette.com/"
     : "http://localhost:4004/",
-    
   "process.env.URL": prod
     ? "https://renatasette.com/"
     : "http://127.0.0.1:3000/"
