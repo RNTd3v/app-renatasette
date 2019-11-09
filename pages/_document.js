10316afe533eca8ca108d7dad9859a89467e5bf7
Renato Andrade <renato@rntdesign.com.br>
2019-11-09 14:11:17 -0300

    Add Favicon

--- pages/_document.js
+++ pages/_document.js
@@ -34,6 +34,13 @@ export default class MyDocument extends Document {
             charset="UTF-8"
             href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/static/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#212222" />
+          <meta name="msapplication-TileColor" content="#212222" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
