4551306d6a650fc3bca98b177250107803ce40cc
Renato Andrade <renato@rntdesign.com.br>
2019-11-25 19:21:41 -0300

    ajustes next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,2 @@
 const withSass = require("@zeit/next-sass");
-const withOffline = require('next-offline');
-const withPlugins = require('next-compose-plugins');
-
-const nextConfig = {};
-
-// module.exports = withSass();
-module.exports = withPlugins([withSass, withOffline], nextConfig);
+module.exports = withSass();
